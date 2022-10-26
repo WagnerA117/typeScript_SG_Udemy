3137de6572067322fb32d4445e4a5299e0692b20
Ahren <ahren@meetingpal.co.za>
2022-10-26 11:30:42 +0200

    firstApplication
    Section 1 of the Complete Developer course to TypeScript

---
+++ fetchjson/index.ts
@@ -0,0 +1,31 @@
+console.log("this was logged");
+
+import axios from "axios";
+
+//in an interface can ignore properties
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  console.log(todo);
+
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logToDo(ID, title, completed);
+});
+
+const logToDo = (id: number, title: string, completed: boolean) => {
+  console.log(
+    ` The id is ${id}, the title is: ${title}, is it finished?: ${completed}`
+  );
+};
